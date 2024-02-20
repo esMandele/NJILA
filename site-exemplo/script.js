c4035417d6b9cddd8b6d51c06adae438d20cb886
esMandele <esperancamandele8@gmail.com>
2024-02-20 11:06:13 +0100

    update continua

--- site-exemplo/script.js
+++ site-exemplo/script.js
@@ -3,3 +3,14 @@ window.addEventListener("scroll", function(){
     let header = document.querySelector('#header')
     header.classList.toggle('rolagem', window.scrollY > 0)
 })
+
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "assets/img/menu_white_36dp.svg";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "assets/img/close_white_36dp.svg";
+    }
+}
